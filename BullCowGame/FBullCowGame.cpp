5d9acb3cd5dab329637c278302e04b09c5dd979e
lorenzoash <z.lakyo@gmail.com>
2019-03-19 20:44:38 -0700

    c++ bull cow game

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,9 @@
+//
+//  FBullCowGame.cpp
+//  BullCowGame
+//
+//  Created by Lorenzo Ash on 3/13/19.
+//  Copyright © 2019 Lorenzo Ash. All rights reserved.
+//
+
+#include "FBullCowGame.hpp"
---
+++ BullCowGame/FBullCowGame.hpp
@@ -0,0 +1,14 @@
+//
+//  FBullCowGame.hpp
+//  BullCowGame
+//
+//  Created by Lorenzo Ash on 3/13/19.
+//  Copyright © 2019 Lorenzo Ash. All rights reserved.
+//
+
+#ifndef FBullCowGame_hpp
+#define FBullCowGame_hpp
+
+#include <stdio.h>
+
+#endif /* FBullCowGame_hpp */
